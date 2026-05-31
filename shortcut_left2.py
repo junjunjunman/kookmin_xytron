cc1001a44cbabc87b98eca9de079d9a198e04887
CMK21 <choimk9911@knu.ac.kr>
2026-05-31 18:15:06 +0900

    Delete shortcut_left2.py

--- shortcut_left2.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-def shortcut_left2_mission():
-    """ 지름길 출구 교차로에서 90도 좌회전을 수행하여 기존 어린이 보호구역 차선으로 궤적 병합 """
-    # TODO: 턴 동작 완료 시점 검증 궤적 도출 
-    is_merged = False
-    
-    if is_merged:
-        return 0.0, 15.0, "passed"
-    return -30.0, 15.0, "turning" 
